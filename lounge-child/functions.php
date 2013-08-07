fe472a30f2edcd819fb1ec4c30832370a28bb341
Brandon Brotsky <brandon@brotskydesigns.com>
2013-08-06 18:54:33 -0700

    Whats up

--- lounge-child/functions.php
+++ lounge-child/functions.php
@@ -6,5 +6,8 @@ Function wp_schools_enqueue_scripts() {
 wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
 // wp_enqueue_style( 'childstyle' );
 exit("This is gay."); // Brandon was wrong
+
+echo "sup dawggggg"; //whats up Michael
+
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts' );
